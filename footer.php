7c1491e3592211cfde1c02cef6d87fd259f1bc3b
Adarshj02 <adarshaudi0000@gmail.com>
2022-06-02 11:41:23 +0530

    patch update

--- footer.php
+++ footer.php
@@ -1,5 +1,4 @@
 
-
     	<script src="<?php echo base_url(); ?>asset/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="<?php echo base_url(); ?>asset/js/scripts.js"></script>
         <script src="<?php echo base_url(); ?>asset/js/cred.js"></script>
